3966ced1955c576b471ad71b63aa8708fa8af903
eatingbug <sj.hong@zipida.com>
2023-02-07 16:24:21 +0900

    Part 1 - 중요한 데이터는 변수말고 state에 담습니다

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  // Destructuring 문법
+  // State 왜 씀? => State 는 html 이랑 다르게 변경되면 자동 렌더링됨. 변경사항이 많은곳에 사용
+  let [글제목, a] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [글내용, b] = useState('2월 17일 발행');
 
-  return (
+  // 리턴문 안에 병렬로 태그를 2개이상 사용 불가
+  return ( 
     <div className="App">
       <div className="black-nav">
-        {/* style 변경시에는 {} 안에 넣어야됨 + 속성명에 - 는 빼기로 인식하므로 붙여야됨 */}
-        <h4 style={ {color : 'red', fontSize : '16px'} }>블로그</h4>
+        <h4>블로그</h4>
       </div>
-      {/* react 에서는 중괄호로 변수를 넣을 수 있음 */}
-      <h4>{ post }</h4>
+      <div className='list'>
+        <h4>{ 글제목[0] }</h4>
+        <p>{ 글내용 }</p>
+        <h4>{ 글제목[1] }</h4>
+        <p>{ 글내용 }</p>
+        <h4>{ 글제목[2] }</h4>
+        <p>{ 글내용 }</p>
+      </div>
     </div>
   );
 }
--- react/react.md
+++ react/react.md
@@ -16,3 +16,12 @@
     1. html 에 class 넣을 땐 className 을 사용한다.
     2. 변수를 html에 넣을 때는 {중괄호} 를 사용
     3. html 에 style 속성을 넣고 싶으면 `<div style={ {color : 'blue', fontSize : '30px'} }> 글씨 </div>` { 속성명 : '속성값' } 방식으로 넣어야된다.
+
+- state
+    - 사용법
+        - `let [a, b] = useState('남자 코트 추천');`
+        - 일반 변수 대신 state 이용하여 잠깐 변수를 저장해둘 수 있다.
+    - 사용하는 이유(#중요)
+        - **state 는 변동사항이 생기면 state 쓰는 html 도 자동으로 재렌더링 해준다.**
+        - 그냥 변수를 html 에 사용하면 변경사항이 자동으로 반영되지 않음
+    
